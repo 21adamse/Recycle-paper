ee71af2701747b5c9acc97f400c15401a7b44d5c
21adamse <EwanAd2010@gmail.com>
2025-04-03 16:58:49 +0100

    Add files via upload

--- Recycle paper/main.py
+++ Recycle paper/main.py
@@ -11,8 +11,8 @@ final_level = 10
 startspeed = 10
 ITEMS = ("battery","bottle","chips","bag")
 
-gameover = False
-gamecomplete = False
+is_game_over = False
+is_game_complete = False
 currentlevel = 1
 items = []
 animations = []
@@ -29,28 +29,74 @@ def get_options_create(extraactors):
     return actors_create
 
 def create_actor(actors_create):
-    pass
+    new_actors=[]
+    for item in actors_create:
+        actor=Actor(item+"img")
+        new_actors.append(actor)
+    return new_actors
 
 def layout_actors(new_actors):
-    pass
+    number_gaps = len(new_actors)+1
+    gap_size = WIDTH // number_gaps
+    random.shuffle(new_actors)
+    for index,item in enumerate(new_actors):
+        new_xpos = (index+1)*gap_size
+        item.x = new_xpos
+
+def animate_actors(new_actors):
+    global animations
+    for actor in new_actors:
+        duration = startspeed-currentlevel
+        actor.anchor = ("center","bottom")
+        animations.append(animate(actor,duration = duration,on_finished = game_over,y = HEIGHT))
+
+def stop_animation(animations):
+    for animation in animations:
+        if animation.running:
+            animation.stop()
 
 def makeitems(extraactors):
     itemscreate = get_options_create(extraactors)
     new_actors = create_actor(itemscreate)
     layout_actors(new_actors)
+    animate_actors(new_actors)
+    return new_actors
+
+def game_complete():
+    global items,currentlevel,animations,is_game_complete
+    stop_animation(animations)
+    if currentlevel == final_level:
+        is_game_complete = True
+    else:
+        currentlevel += 1
+        items=[]
+        animations=[]
+
+def game_over():
+    global is_game_over
+    is_game_over = True
 
 def update():
     global items
     if len(items) == 0:
         items = makeitems(currentlevel)
 
+def on_mouse_down(pos):
+    global items, currentlevel
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                game_complete()
+            else:
+                game_over()
+
 def draw():
     global items, currentlevel, gameover, gamecomplete
     screen.clear()
     screen.blit("bground",(0,0))
-    if gameover:
+    if is_game_over:
         displaymessage("Game over","You lose")
-    elif gamecomplete:
+    elif is_game_complete:
         displaymessage("Game complete","You win")
     else:
         for item in items:
